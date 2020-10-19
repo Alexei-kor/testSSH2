6efdc9c22a39711b9ef1b36d7087ebcf7f9d3729
Alexei <kornjushenkov@mail.ru>
2020-10-19 23:12:47 +0300

    initial commit

---
+++ Dynamic Web Project/src/ru/database/DatabaseExample.java
@@ -0,0 +1,38 @@
+package ru.database;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+
+public class DatabaseExample {
+	public static List<String> getBooks() {
+		List<String> result = new ArrayList<String>();
+		
+		try (Connection connection = DriverManager.getConnection("jdbc:postgresql://localhost:5432/postgres", "postgres", "7895123")){
+			
+			System.out.println("My first example by jdbc");
+			
+			Statement statement = connection.createStatement();
+			
+			ResultSet resultSet = statement.executeQuery("Select * from books.books");
+			while (resultSet.next()) {
+				result.add(resultSet.getString("name"));
+			
+			}
+			
+		}
+		
+		catch (SQLException e) {
+			System.out.println("Fail");
+			e.printStackTrace();
+		}
+		
+		return result;
+		
+		
+	}
+}
